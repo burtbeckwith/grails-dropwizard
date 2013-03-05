6d2b5b67647c225bb081da84c93fad3576f37569
Burt Beckwith <burt@burtbeckwith.com>
2013-03-04 22:18:59 -0500

    made the service class configurable and added error handling

--- scripts/RunDropwizard.groovy
+++ scripts/RunDropwizard.groovy
@@ -1,17 +1,34 @@
+import grails.util.GrailsUtil
+
 includeTargets << grailsScript('_GrailsBootstrap')
 
 target(runDropwizard: 'The description of the script goes here!') {
 	depends checkVersion, configureProxy, bootstrap, loadApp
 
-	String yamlPath = config.grails.plugin.dropwizard.yamlPath ?: 'classpath:dropwizard.yml'
-	def resource = appCtx.getResource(yamlPath)
-	if (!resource.exists()) {
-		println "ERROR: YAML config file '$yamlPath' not found"
-		return
-	}
+	try {
+		def conf = config.grails.plugin.dropwizard
 
-	def service = classLoader.loadClass('grails.plugin.dropwizard.GrailsService').newInstance(appCtx, resource.file)
-	service.start()
+		String yamlPath = conf.yamlPath ?: 'classpath:dropwizard.yml'
+		def resource = appCtx.getResource(yamlPath)
+		if (!resource.exists()) {
+			println "ERROR: YAML config file '$yamlPath' not found"
+			return
+		}
+
+		String serviceClassName = conf.serviceClassName ?: 'grails.plugin.dropwizard.GrailsService'
+
+		try {
+			def service = classLoader.loadClass(serviceClassName).newInstance(appCtx, resource.file)
+			service.start()
+		}
+		catch (ex) {
+			println "Unable to instantiate the specified service class: $ex.message"
+			GrailsUtil.deepSanitize(ex).printStackTrace(System.out)
+		}
+	}
+	catch (e) {
+		GrailsUtil.deepSanitize(e).printStackTrace(System.out)
+	}
 }
 
 setDefaultTarget 'runDropwizard'
